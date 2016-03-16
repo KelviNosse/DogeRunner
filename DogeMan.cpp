ef08f608fdfd3bd4c4d5621a8ea53e4c404d5edd
Kelvin Nose <kelvinjosse@gmail.com>
2016-03-15 20:37:22 -0600

    Implementing initial functions for DogeMan object

--- DogeMan.cpp
+++ DogeMan.cpp
@@ -1,16 +1,34 @@
 #include "DogeMan.h"
-
+#include <stdio.h>
+#include <string.h>
 DogeMan::DogeMan(char* name)
 {
     this->name = name;
     this->alive = true;
     this->lives = 3;
     this->score = 0;
+
+    this->CreateBody();
 }
 
 DogeMan::~DogeMan()
 {
     //dtor
+}
+
+void DogeMan::CreateBody(){
+    strcpy(this->body, "..___x");
+    strcat(this->body, "/  V`-,x");
+    strcat(this->body, "\\__.~~x");
+    strcat(this->body, "/-|--/x");
+    strcat(this->body, " /--ix");
+    strcat(this->body, "/   Lx");
+    strcat(this->body, "L");
+
+}
+
+char* DogeMan::GetBody(){
+    return this->body;
 }
 
 char* DogeMan::GetName(){
